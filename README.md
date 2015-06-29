4de5a8b5db655207372553722d4e9287eff5e365
Segundo Fdez <segun.2@gmail.com>
2015-06-29 23:20:51 +0200

    Add initial content

---
+++ README.md
@@ -0,0 +1,14 @@
+# CSS4 COLORS
+
+Named colors based on CSS Color Module Level 4 [reticulas-cssframework][1]
+
+
+## Gulp tasks
+
+1.  Generate colors on dist folder
+
+    ```bash
+    gulp colors
+    ```
+
+[1]:http://dev.w3.org/csswg/css-color/#named-colors
---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+
+/**
+* Gulp errors
+*/
+function swallowError (error) {
+    console.log(error.toString());
+    this.emit('end');
+}
+
+/**
+* Task custom colors
+*/
+gulp.task('colors', function () {
+    var less_src_import = 'css4-colors.less';
+    var less_dest_folder = 'dist/';
+
+    return gulp.src(less_src_import)
+        .pipe(less())
+        .on('error', swallowError)
+        .pipe(gulp.dest(less_dest_folder))
+});
