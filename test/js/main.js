15fd70737fb5b7ef3db7d2520c2545640b7148d8
Segundo Fdez <segun.2@gmail.com>
2018-02-17 01:02:06 +0100

    update dependencies and update js

--- test/js/main.js
+++ test/js/main.js
@@ -1,19 +1,58 @@
-$(document).ready(function() {
+$(function() {
 
-    // JSON to list
+    var value0 = 0;
+    var value1 = 1;
+    var value2 = 2;
+    var value3 = 3;
+    var value4 = 4;
+    var value5 = 5;
+    var value6 = 6;
+    var value7 = 7;
+    var value8 = 8;
+    var value9 = 9;
+
+    var valueA = 10;
+    var valueB = 11;
+    var valueC = 12;
+    var valueD = 13;
+    var valueE = 14;
+    var valueF = 15;
+
+    // fe=254(Red), fa=250(Green), fd=253(Blue)
+    // Calculando el valor decimal ( FE )
+    // 1 - Transformamos el primer numero en su equivalente en base 16 y lo multiplicaremos por 16.
+    // F=15;
+    // 15*16 = 240;
+    // 2 - El segundo dígito ( E ) representa las unidades.
+    // Según la tabla de equivalencias E = 14
+    // 3 - Sumamos las unidades (E) al valor obtenido:
+    // 240+14=254;
+
+
+    var redArrayDec = [];
+
     $.getJSON( "../css4-colors.json", function( data ) {
-
         var items = [];
-
+        // var hexArrayfirst = [];
+        // var hexArraysecond = [];
         for (var i = 0; i < 148; i++) {
-            $.each(data, function (key, val) {
+            $.each( data, function( key, val ) {
                 items.push('<li id="' + val[i].id + '"class="background-color-' + val[i].id + '">' + val[i].attributes.color_name + ' <span>' + val[i].attributes.hex_rgb + ' | ' + val[i].attributes.decimal + '</span></li>');
+                // hexArrayfirst.push(val[i].attributes.hex_rgb.charAt(1));
+                // hexArraysecond.push(val[i].attributes.hex_rgb.substr(1,2));
+
+                redArrayDec.push(val[i].attributes.decimal.substr(0, val[i].attributes.decimal.indexOf(',')));
             });
         }
+        // console.log(hexArrayfirst);
+        // console.log(hexArraysecond);
+
         $( "<ul/>", {
             "id": "sort-list",
             html: items.join( "" )
         }).appendTo( ".content" );
+
+        console.log(redArrayDec);
     });
 
     // A-Z  Z-A
@@ -36,5 +75,15 @@ $(document).ready(function() {
         e.preventDefault();
     });
 
-
+    // Red Color
+    // $('.sort-red').click(function(e) {
+    //     redArrayDec.sort(function(a,b) {
+    //         if (isNaN(a) || isNaN(b)) {
+    //             return a > b ? 1 : -1;
+    //         }
+    //         return a - b;
+    //     });
+    //     e.preventDefault();
+    //     console.log(redArrayDec);
+    // });
 });
